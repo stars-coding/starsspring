04a47d1533ee24e4075a586f7a4238af9f93d883
stars-coding <2715800477@qq.com>
2023-04-19 21:06:54 +0800

    AOP开发

---
+++ src/main/java/com/stars/starsspring/framework/aop/AfterAdvice.java
@@ -0,0 +1,21 @@
+package com.stars.starsspring.framework.aop;
+
+import org.aopalliance.aop.Advice;
+
+/**
+ * 后置通知——接口
+ * <p>
+ * <p>
+ * 属性字段：
+ * <p>
+ * 重写方法：
+ * <p>
+ * 定义方法：
+ * <p>
+ * 编写方法：
+ *
+ * @author stars
+ */
+public interface AfterAdvice extends Advice {
+
+}
